b934b31209e4b2ece04a77184bff881c561323b3
CW01\uidq5344 <florentin.beltechi@conti-engineering.com>
2021-10-26 00:21:08 +0300

    add stub for Percolation class

--- Part I Week 1/Union-Find/percolation/Percolation.java
+++ Part I Week 1/Union-Find/percolation/Percolation.java
@@ -4,26 +4,53 @@
  *  Last modified:     1/1/2019
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdOut;
+
 public class Percolation {
-    
+
     // creates n-by-n grid, with all sites initially blocked
-    public Percolation(int n)
+    public Percolation(int n) {
+
+    }
 
     // opens the site (row, col) if it is not open already
-    public void open(int row, int col)
+    public void open(int row, int col) {
+
+    }
 
     // is the site (row, col) open?
-    public boolean isOpen(int row, int col)
+    public boolean isOpen(int row, int col) {
+        boolean res = false;
+
+        return res;
+    }
 
     // is the site (row, col) full?
-    public boolean isFull(int row, int col)
+    public boolean isFull(int row, int col) {
+        boolean res = false;
+
+        return res;
+    }
 
     // returns the number of open sites
-    public int numberOfOpenSites()
+    public int numberOfOpenSites() {
+        int res = 0;
+
+        return res;
+    }
 
     // does the system percolate?
-    public boolean percolates()
+    public boolean percolates() {
+        boolean res = false;
+
+        return false;
+    }
 
     // test client (optional)
-    public static void main(String[] args)
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        Percolation grid = new Percolation(n);
+
+        StdOut.println("Hello");
+    }
 }
