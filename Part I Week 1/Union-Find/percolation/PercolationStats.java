c2192f559cf6c1671a1eeb7598eea5e89402df38
CW01\uidq5344 <florentin.beltechi@conti-engineering.com>
2021-11-01 22:18:36 +0200

    add percolation assigment

--- Part I Week 1/Union-Find/percolation/PercolationStats.java
+++ Part I Week 1/Union-Find/percolation/PercolationStats.java
@@ -5,52 +5,69 @@
  **************************************************************************** */
 
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
-    private int szLattice;
-
+    private static final double CONFIDENCE_95 = 1.96;
+    private final int count;
+    private final double[] results;
 
     // perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials) {
         if (n <= 0 || trials <= 0) {
-            throw new IllegalArgumentException("n or trials cannot be ≤ 0");
+            throw new IllegalArgumentException("n or trials ≤ 0!");
+        }
+        int gridSize = n;
+        count = trials;
+        results = new double[count];
+
+        for (int trial = 0; trial < count; trial++) {
+            Percolation percolation = new Percolation(gridSize);
+            while (!percolation.percolates()) {
+                int row = StdRandom.uniform(1, gridSize + 1);
+                int col = StdRandom.uniform(1, gridSize + 1);
+                percolation.open(row, col);
+            }
+            int openSites = percolation.numberOfOpenSites();
+            double result = (double) openSites / (gridSize * gridSize);
+            results[trial] = result;
         }
 
     }
 
     // sample mean of percolation threshold
     public double mean() {
-        //return StdStats.mean();
-        return 0.0; // todo remove
+        return StdStats.mean(results);
     }
 
     // sample standard deviation of percolation threshold
     public double stddev() {
-        //return StdStats.stddev();
-        return 0.0; // todo remove
+        return StdStats.stddev(results);
     }
 
     // low endpoint of 95% confidence interval
     public double confidenceLo() {
-
-        return 0.0; // todo remove
+        return mean() - ((CONFIDENCE_95 * stddev()) / Math.sqrt(count));
     }
 
     // high endpoint of 95% confidence interval
     public double confidenceHi() {
-
-        return 0.0; // todo remove
+        return mean() + ((CONFIDENCE_95 * stddev()) / Math.sqrt(count));
     }
 
     // test client (see below)
     public static void main(String[] args) {
-        int n = 1;
-        int trails = 1;
+        int n = 10;
+        int trails = 10;
         if (args.length >= 2) {
             n = Integer.parseInt(args[0]);
             trails = Integer.parseInt(args[1]);
         }
-        PercolationStats lattice = new PercolationStats(n, trails);
-        StdOut.println("Hello");// todo remove
+        PercolationStats ps = new PercolationStats(n, trails);
+        String confidence = ps.confidenceLo() + ", " + ps.confidenceHi();
+        StdOut.println("mean                    = " + ps.mean());
+        StdOut.println("stddev                  = " + ps.stddev());
+        StdOut.println("95% confidence interval = [" + confidence + "]");
     }
 }
